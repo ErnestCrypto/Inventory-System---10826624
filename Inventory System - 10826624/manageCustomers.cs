24022cb01c3279a74f2d6455f09d0c5c455f34a1
ErnestCrypto <84491282+ErnestCrypto@users.noreply.github.com>
2022-08-27 17:49:39 +0000

    Add files via upload

---
+++ Inventory System - 10826624/manageCustomers.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Inventory_System___10826624
+{
+    public partial class manageCustomers : Form
+    {
+        public manageCustomers()
+        {
+            InitializeComponent();
+        }
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Admin\Documents\inventorydb.mdf;Integrated Security=True;Connect Timeout=30");
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void unameTb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PasswordTb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fnameTb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void manageCustomers_Load(object sender, EventArgs e)
+        {
+            populate();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsersGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Customerid.Text = CustomersGV.SelectedRows[0].Cells[0].Value.ToString();
+            CustomernameTb.Text = CustomersGV.SelectedRows[0].Cells[1].Value.ToString();
+            CustomerPhoneTb.Text = CustomersGV.SelectedRows[0].Cells[2].Value.ToString();
+        }
+
+        void populate()
+        {
+            try
+            {
+                Con.Open();
+                string Myquery = "select * from CustomerTb1";
+                SqlDataAdapter da = new SqlDataAdapter(Myquery, Con);
+                SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                var ds = new DataSet();
+                da.Fill(ds);
+                CustomersGV.DataSource = ds.Tables[0];
+                Con.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        private void buton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Con.Open();
+                SqlCommand cmd = new SqlCommand("insert into CustomerTb1 values('" + Customerid.Text + "','" + CustomernameTb.Text + "','" + CustomerPhoneTb.Text + "')", Con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Customer has been Successfully Added");
+                Con.Close();
+                populate();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Customerid.Text == "")
+            {
+                MessageBox.Show(" Enter the customer's Id");
+
+            }
+            else
+            {
+                Con.Open();
+                string Myquery = "delete from CustomerTb1 where CustId ='" + Customerid.Text + "' ;";
+                SqlCommand cmd = new SqlCommand(Myquery, Con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Customer successfully Deleted");
+                Con.Close();
+                populate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Con.Open();
+                SqlCommand cmd = new SqlCommand("update CustomerTb1 set CustName= '" + CustomernameTb.Text + "',CustPhone = '" + CustomerPhoneTb.Text +  "' where CustId = '" + Customerid.Text + "' ", Con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Customer has been Successfully Updated");
+                Con.Close();
+                populate();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
