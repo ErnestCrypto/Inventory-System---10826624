2f76fdb1126b7f8d240ca1d58a84d3949e1f057e
ErnestCrypto <84491282+ErnestCrypto@users.noreply.github.com>
2022-08-27 20:02:47 +0000

    Add files via upload

---
+++ Inventory System - 10826624/manageCategories.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Inventory_System___10826624
+{
+    partial class manageCategories
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.CatNameTb = new System.Windows.Forms.TextBox();
+            this.CatIdTb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CategoriesGV = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buton1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CategoriesGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Crimson;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(780, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(112, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(545, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "INVENTORY MANAGEMENT SYSTEM";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(234, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MANAGE CATEGORIES";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Crimson;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 524);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 26);
+            this.panel2.TabIndex = 19;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // CatNameTb
+            // 
+            this.CatNameTb.BackColor = System.Drawing.Color.White;
+            this.CatNameTb.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CatNameTb.ForeColor = System.Drawing.Color.Crimson;
+            this.CatNameTb.Location = new System.Drawing.Point(12, 146);
+            this.CatNameTb.Multiline = true;
+            this.CatNameTb.Name = "CatNameTb";
+            this.CatNameTb.Size = new System.Drawing.Size(159, 34);
+            this.CatNameTb.TabIndex = 21;
+            this.CatNameTb.Text = "CategoryName";
+            // 
+            // CatIdTb
+            // 
+            this.CatIdTb.BackColor = System.Drawing.Color.White;
+            this.CatIdTb.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CatIdTb.ForeColor = System.Drawing.Color.Crimson;
+            this.CatIdTb.Location = new System.Drawing.Point(12, 106);
+            this.CatIdTb.Multiline = true;
+            this.CatIdTb.Name = "CatIdTb";
+            this.CatIdTb.Size = new System.Drawing.Size(159, 34);
+            this.CatIdTb.TabIndex = 20;
+            this.CatIdTb.Text = "CategoryId";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label4.Location = new System.Drawing.Point(352, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(226, 33);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "CATEGORIES LIST";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // CategoriesGV
+            // 
+            this.CategoriesGV.AllowUserToAddRows = false;
+            this.CategoriesGV.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.CategoriesGV.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.CategoriesGV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CategoriesGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.CategoriesGV.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.CategoriesGV.BackgroundColor = System.Drawing.Color.White;
+            this.CategoriesGV.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SunkenHorizontal;
+            this.CategoriesGV.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Crimson;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Crimson;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Crimson;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.CategoriesGV.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.CategoriesGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.CategoriesGV.DefaultCellStyle = dataGridViewCellStyle3;
+            this.CategoriesGV.Location = new System.Drawing.Point(177, 139);
+            this.CategoriesGV.Name = "CategoriesGV";
+            this.CategoriesGV.ReadOnly = true;
+            this.CategoriesGV.RowTemplate.Height = 25;
+            this.CategoriesGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.CategoriesGV.Size = new System.Drawing.Size(611, 360);
+            this.CategoriesGV.TabIndex = 22;
+            this.CategoriesGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CategoriesGV_CellContentClick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Crimson;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(12, 464);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 35);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "HOME";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 35);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 35);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "EDIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buton1
+            // 
+            this.buton1.BackColor = System.Drawing.Color.Crimson;
+            this.buton1.FlatAppearance.BorderSize = 0;
+            this.buton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buton1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buton1.ForeColor = System.Drawing.Color.White;
+            this.buton1.Location = new System.Drawing.Point(12, 283);
+            this.buton1.Name = "buton1";
+            this.buton1.Size = new System.Drawing.Size(159, 35);
+            this.buton1.TabIndex = 24;
+            this.buton1.Text = "ADD";
+            this.buton1.UseVisualStyleBackColor = false;
+            this.buton1.Click += new System.EventHandler(this.buton1_Click);
+            // 
+            // manageCategories
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 550);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CategoriesGV);
+            this.Controls.Add(this.CatNameTb);
+            this.Controls.Add(this.CatIdTb);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "manageCategories";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "manageCatogories";
+            this.Load += new System.EventHandler(this.manageCategories_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CategoriesGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Panel panel2;
+        private TextBox CatNameTb;
+        private TextBox CatIdTb;
+        private Label label4;
+        private DataGridView CategoriesGV;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button buton1;
+    }
+}
---
+++ Inventory System - 10826624/manageCategories.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Inventory_System___10826624
+{
+    public partial class manageCategories : Form
+    {
+        public manageCategories()
+        {
+            InitializeComponent();
+        }
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Admin\Documents\inventorydb.mdf;Integrated Security=True;Connect Timeout=30");
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void CategoriesGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            CatIdTb.Text = CategoriesGV.SelectedRows[0].Cells[0].Value.ToString();
+            CatNameTb.Text = CategoriesGV.SelectedRows[0].Cells[1].Value.ToString();
+        }
+
+        void populate()
+        {
+            try
+            {
+                Con.Open();
+                string Myquery = "select * from CategoryTb1";
+                SqlDataAdapter da = new SqlDataAdapter(Myquery, Con);
+                SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                var ds = new DataSet();
+                da.Fill(ds);
+                CategoriesGV.DataSource = ds.Tables[0];
+                Con.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void buton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Con.Open();
+                SqlCommand cmd = new SqlCommand("insert into CategoryTb1 values('" + CatIdTb.Text + "','" + CatNameTb.Text + "')", Con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Customer has been Successfully Added");
+                Con.Close();
+                populate();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (CatIdTb.Text == "")
+            {
+                MessageBox.Show(" Enter the category's Id");
+
+            }
+            else
+            {
+                Con.Open();
+                string Myquery = "delete from CategoryTb1 where CatId ='" + CatIdTb.Text + "' ;";
+                SqlCommand cmd = new SqlCommand(Myquery, Con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Category successfully Deleted");
+                Con.Close();
+                populate();
+            }
+        }
+
+        private void manageCategories_Load(object sender, EventArgs e)
+        {
+            populate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Con.Open();
+                SqlCommand cmd = new SqlCommand("update CategoryTb1 set CatName= '" + CatNameTb.Text + "',CatId = '" + CatIdTb.Text + "' where CatId = '" + CatIdTb.Text + "' ", Con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Category has been Successfully Updated");
+                Con.Close();
+                populate();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
